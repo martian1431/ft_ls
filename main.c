6eef177703cfdfc8991d73fecd53203d453ceba3
Phetho <pmalope@student.wethinkcode.co.za>
2019-08-27 14:39:00 +0200

    updated main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pmalope <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 15:22:56 by pmalope           #+#    #+#             */
-/*   Updated: 2019/08/27 14:19:49 by pmalope          ###   ########.fr       */
+/*   Updated: 2019/08/27 14:37:48 by pmalope          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,8 @@ int		main(int argc, char **argv)
 	if (path == NULL)
 		path = ft_lstnew(".", ft_strlen("."));
 	ft_open_dir(opt, path, path->next != NULL ? 1 : 0);
-	free_lists(path);
+//	free_lists(path);
+	/*remove*/
 	sleep(100);
 	return (0);
 }
